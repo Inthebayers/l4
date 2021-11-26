622ba73251d1fa11ec4e7179f4081bd2ea0a5d27
Carpetcruiser <59855241+Carpetcruiser@users.noreply.github.com>
2021-11-26 15:12:15 -0800

    book.cpp upload

---
+++ Book.cpp
@@ -0,0 +1,120 @@
+//---------------------------------------------------------------------------
+// book.h
+//---------------------------------------------------------------------------
+// A book object. Handles the creation and storage of a
+// book, containing title, author, number of copies "owned" by the library,
+// and copies of this book currently checked in. Parent class of fictionbook,
+// periodicalbook, and childrenbook.
+//
+//---------------------------------------------------------------------------
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Book {
+
+    /**
+     * @brief Output operator overload for PeriodicalBook object. Dictates
+     * output formatting and functionality for a periodical book.
+     *
+     * @return ostream& containing information to be output in a formatted style
+     * @pre: Assumes valid FictionBook object being passed in.
+     * @post: Data not changed, but is output.
+     */
+    friend ostream& operator<<(ostream&, const Book&);
+
+public:
+    /**
+     * @brief Construct a new Book object
+     *
+     * @param title of book being created
+     * @param bookCode code of book being created to determine type of book
+     */
+    Book(string title, char bookCode) {
+        // set title to incoming title
+        this->title = title;
+
+
+
+    }
+
+    //---------------------------------------------------------------------------
+     /**
+      * @brief Destroy the Book object
+      *
+      * @pre: Assumes valid object has been created
+      * @post: Object will be deleted and memory freed.
+      */
+    virtual ~Book();
+
+    //---------------------------------------------------------------------------
+     /**
+      * @brief Handles the field management required to check out a book.
+      *
+      * @return true if check out was successful
+      * @return false if check out was not sucessful
+      *
+      * @pre: Assumes book has amount to check out
+      * @post: Book will remove 1 copy of itself from the library stores
+      */
+    bool checkOut();
+
+    //---------------------------------------------------------------------------
+     /**
+      * @brief Get the title of the current book object.
+      *
+      * @return string containing the title of the current book
+      *
+      * @pre: Assumes valid book with title has been created.
+      * @post: No changes.
+      */
+    string getTitle() const;
+
+    //---------------------------------------------------------------------------
+     /**
+      * @brief Returns book copy to library, adjusting values for currently
+      * checked in copies.
+      *
+      * @return true if value is modified
+      * @return false if value is not modified
+      *
+      * @pre: Assumes book is checked out
+      * @post: Book is returned to library, incrementing the number of copies
+      */
+    bool returnToLibrary();
+
+    //---------------------------------------------------------------------------
+     /**
+      * @brief Get the code/genre of the book for hashing purposes.
+      *
+      * @return char corresponding to the code/genre of the book.
+      *
+      * @pre: Assumes valid code and properly created book.
+      * @post: No changes to data.
+      */
+    char getBookCode() const;
+
+    virtual bool operator<(const Book*& rhsBook) = 0; 
+
+    virtual bool operator>(const Book*& rhsBook) = 0; 
+
+    virtual bool operator==(const Book*& rhsBook) = 0; 
+
+    virtual bool operator!=(const Book*& rhsBook) = 0; 
+
+private:
+    // title of book
+    string title;
+
+    // code of book
+    char bookCode;
+
+    // copies of book checked into library
+    int numberCheckedIn;
+
+    // total amount of copies the library owns
+    int totalTitlesOwned;
+
+    // default booktype
+    char bookType = 'H';
+};
