f76050b67538cd57dc36ef56c43e1a4833bc27c0
Carpetcruiser <59855241+Carpetcruiser@users.noreply.github.com>
2021-11-28 16:56:39 -0800

    Book changes for get/set

--- Book.cpp
+++ Book.cpp
@@ -13,4 +13,65 @@
 using namespace std;
 
 Book::Book()
-{}
+{
+}
+
+void Book::setTitle(string incomingTitle)
+{
+   title_ = incomingTitle;
+}
+
+string Book::getTitle()
+{
+   return title_;
+}
+
+void Book::setBookCode(char incomingChar)
+{
+   bookCode_ = incomingChar;
+}
+
+char Book::getBookCode()
+{
+   return bookCode_;
+}
+
+void Book::setAvailableCopies(int newNumberOfCopies)
+{
+   availableCopies_ = newNumberOfCopies;
+}
+
+int Book::getAvailableCopies()
+{
+   return availableCopies_;
+}
+
+bool Book::checkOutOneBook()
+{
+   if (availableCopies_ > 0)
+   {
+      availableCopies_ = availableCopies_ - 1;
+      return true;
+   }
+   return false;
+}
+
+bool Book::returnOneBook()
+{
+   if (availableCopies_ < totalCopies_)
+   {
+      availableCopies_ += 1;
+   }
+}
+
+bool Book::setTotalCopies(int newTotalCopyAmount)
+{
+   if (newTotalCopyAmount > 0)
+   {
+      totalCopies_ = newTotalCopyAmount;
+   }
+}
+
+int Book::getTotalCopies() {
+   return totalCopies_;
+}
--- book.h
+++ book.h
@@ -13,32 +13,33 @@
 
 using namespace std;
 
-class Book : public Item {
+class Book : public Item
+{
 public:
-    ///**
-    // * @brief Construct a new Book object
-    // *
-    // * @param title of book being created
-    // * @param bookCode code of book being created to determine type of book
-    // */
-    //Book(string title, char bookCode);
+   ///**
+   // * @brief Construct a new Book object
+   // *
+   // * @param title of book being created
+   // * @param bookCode code of book being created to determine type of book
+   // */
+   //Book(string title, char bookCode);
 
-    /**
+   /**
      * Default constructor
      */
-    Book();
+   Book();
 
-    //---------------------------------------------------------------------------
-     /**
+   //---------------------------------------------------------------------------
+   /**
       * @brief Destroy the Book object
       *
       * @pre: Assumes valid object has been created
       * @post: Object will be deleted and memory freed.
       */
-    virtual ~Book();
+   virtual ~Book();
 
-    //---------------------------------------------------------------------------
-     /**
+   //---------------------------------------------------------------------------
+   /**
       * @brief Handles the field management required to check out a book.
       *
       * @return true if check out was successful
@@ -47,10 +48,10 @@ public:
       * @pre: Assumes book has amount to check out
       * @post: Book will remove 1 copy of itself from the library stores
       */
-    virtual bool checkOut() = 0;
+   virtual bool checkOut() = 0;
 
-    //---------------------------------------------------------------------------
-     /**
+   //---------------------------------------------------------------------------
+   /**
       * @brief Get the title of the current book object.
       *
       * @return string containing the title of the current book
@@ -58,10 +59,10 @@ public:
       * @pre: Assumes valid book with title has been created.
       * @post: No changes.
       */
-    virtual string getTitle() const = 0;
+   virtual string getTitle() const = 0;
 
-    //---------------------------------------------------------------------------
-     /**
+   //---------------------------------------------------------------------------
+   /**
       * @brief Returns book copy to library, adjusting values for currently
       * checked in copies.
       *
@@ -71,93 +72,108 @@ public:
       * @pre: Assumes book is checked out
       * @post: Book is returned to library, incrementing the number of copies
       */
-    virtual bool returnToLibrary() = 0;
+   virtual bool returnToLibrary() = 0;
 
-    //---------------------------------------------------------------------------
-     /**
+   //---------------------------------------------------------------------------
+   /**
       * @brief Get the code/genre of the book for hashing purposes.
       *
-      * @return char corresponding to the code/genre of the book.
+      * @return char corresponding to the code/genre of the book. hell and now we're going past 80
       *
       * @pre: Assumes valid code and properly created book.
       * @post: No changes to data.
       */
-    virtual char getBookCode() const = 0;
+   virtual char getBookCode() const = 0;
 
-    /**
+   /**
      * Gets the title of the given book. Pure virtual.
      */
-    virtual string getTitle() = 0;
+   virtual string getTitle();
 
-    /**
+   /**
      * Sets the title of the given book. Pure virtual.
      */
-    virtual void setTitle() = 0;
+   virtual void setTitle(string incomingTitle);
 
-    /** display
+   virtual void setBookCode(char);
+
+   virtual void setAvailableCopies(int);
+
+   virtual int getAvailableCopies();
+   virtual char getBookCode();
+
+   virtual bool checkOutOneBook();
+
+   virtual bool returnOneBook();
+
+   virtual bool setTotalCopies(int);
+
+   virtual int getTotalCopies();
+
+   /** display
      * @brief prints the contents of the book to consoel,
      * pure virtual function
      * @pre implemented display() in child classes
      * @post book contents displayed to console
      */
-    virtual void display() = 0;
+   virtual void display() = 0;
 
-    /** create
+   /** create
      * @brief creates a new book object, pure virutal funciton
      * @pre implemented create() in child classes
      * @return returns a pointer to a new book object
      */
-    virtual Book* create() = 0;
+   virtual Book *create() = 0;
 
-    /**
+   /**
     * Less than operator. Compares LHS and RHS for lesser value.
     * @param rhsBook rhs Book being passed in
     * @return true
     * @return false
     */
-    virtual bool operator<(const Book*& rhsBook) = 0;
+   virtual bool operator<(const Book *&rhsBook) = 0;
 
-    /**
+   /**
      *
      * Greater than operator. compares LHS and RHS for greater value.
      * @param rhsBook rhs Book being passed in for comparison
      * @return true
      * @return false
      */
-    virtual bool operator>(const Book*& rhsBook) = 0;
+   virtual bool operator>(const Book *&rhsBook) = 0;
 
-    /**
+   /**
      * Equals operator. Determines if two Book objects are equal.
      *
      * @param rhsBook rhs Book being passed in for equals comparison
      * @return true
      * @return false
      */
-    virtual bool operator==(const Book*& rhsBook) = 0;
+   virtual bool operator==(const Book *&rhsBook) = 0;
 
-    /**
+   /**
      * Does not equals operator. Determines if two books are not equal.
      *
      * @param rhsBook
      * @return true
      * @return false
      */
-   
-   virtual bool operator!=(const Book*& rhsBook) = 0;
+
+   virtual bool operator!=(const Book *&rhsBook) = 0;
 
 protected:
-    // title of book
-    string title_;
+   // title of book
+   string title_;
 
-    // code of book
-    char bookCode_;
+   // code of book
+   char bookCode_;
 
-    // copies of book checked into library
-    int availableCopies_;
+   // copies of book checked into library
+   int availableCopies_;
 
-    // total amount of copies the library owns
-    int totalCopies_;
+   // total amount of copies the library owns
+   int totalCopies_;
 
-    // default booktype
-    char bookType_ = 'H';
+   // default booktype
+   char bookType_ = 'H';
 };
