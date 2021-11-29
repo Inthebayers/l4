3d011cb15d31a97783b113f9e9253aed5d6d22dd
Brennan Ayers <Inthebayers@gmail.com>
2021-11-29 14:02:16 -0800

    childrenbook buildBook and setters fix

--- childrenbook.cpp
+++ childrenbook.cpp
@@ -4,125 +4,133 @@
 using namespace std;
 //---------------------------------------------------------------------------
 // Constructor
-ChildrenBook::ChildrenBook()
-{
-    author_ = "";
-    year_ = 0;
+ChildrenBook::ChildrenBook() {
+  author_ = "";
+  year_ = 0;
 }
 
 //---------------------------------------------------------------------------
 // Destrucor
-ChildrenBook::~ChildrenBook()
-{
-}
+ChildrenBook::~ChildrenBook() {}
 
 //---------------------------------------------------------------------------
 // buildBook
-// TODO implement
-bool ChildrenBook::buildBook(istream& in) {
 
+bool ChildrenBook::buildBook(istream &inFile) {
+  string title = "";
+  string author = "";
+  int year;
+
+  inFile.get();
+
+  getline(inFile, author, ',');
+  inFile.get(); // remove blank space
+  getline(inFile, title, ',');
+  inFile.get(); // remove blank space
+  inFile >> year;
+  setAuthor(author);
+  setTitle(title);
+
+  return (setYear(year));
 }
 
 //---------------------------------------------------------------------------
 // create
-Book* ChildrenBook::create()
-{
-    return new ChildrenBook();
-}
+Book *ChildrenBook::create() { return new ChildrenBook(); }
 
 //---------------------------------------------------------------------------
 // setAuthor
-void ChildrenBook::setAuthor(string author)
-{
-    author_ = author;
-}
+void ChildrenBook::setAuthor(string author) { author_ = author; }
 
 //---------------------------------------------------------------------------
 // getAuthor
-string ChildrenBook::getAuthor() const
-{
-    return author_;
-}
+string ChildrenBook::getAuthor() const { return author_; }
 
 //---------------------------------------------------------------------------
 // setYear
-void ChildrenBook::setYear(int year) {
+bool ChildrenBook::setYear(int year) {
+  if (year > 0) {
     year_ = year;
+    return true;
+  }
+  return false;
 }
 
 //---------------------------------------------------------------------------
 // getYear
-int ChildrenBook::getYear() const {
-    return year_;
-}
+int ChildrenBook::getYear() const { return year_; }
 
 //---------------------------------------------------------------------------
 // display
 // TODO: Add spacing
 void ChildrenBook::display() const {
-    cout << title_ << author_ << year_ << endl;
+  cout << title_ << author_ << year_ << endl;
 }
 
 //---------------------------------------------------------------------------
-// overloaded comparison operators 
+// overloaded comparison operators
 // Children sorted by title, then author
 //---------------------------------------------------------------------------
 
-
 //---------------------------------------------------------------------------
-// overloaded operator < 
-bool ChildrenBook::operator<(const Book& rhsBook) const {
-    bool holder = false; 
+// overloaded operator <
+bool ChildrenBook::operator<(const Book &rhsBook) const {
+  bool holder = false;
 
-    // cast book to children's book 
-    const ChildrenBook& rhsBookCasted = static_cast<const ChildrenBook&>(rhsBook);
+  // cast book to children's book
+  const ChildrenBook &rhsBookCasted =
+      static_cast<const ChildrenBook &>(rhsBook);
 
-    // compare titles
-    (getTitle() < rhsBookCasted.getTitle()) ? holder = true : holder = false;
+  // compare titles
+  (getTitle() < rhsBookCasted.getTitle()) ? holder = true : holder = false;
 
-    // if the book titles are the same compare authors
-    if (getTitle() == rhsBookCasted.getTitle()) {
-        (getAuthor() < rhsBookCasted.getAuthor()) ? holder = true : holder = false;
-    }
-    return holder;
+  // if the book titles are the same compare authors
+  if (getTitle() == rhsBookCasted.getTitle()) {
+    (getAuthor() < rhsBookCasted.getAuthor()) ? holder = true : holder = false;
+  }
+  return holder;
 }
 
 //---------------------------------------------------------------------------
 // overloaded operator>
-bool ChildrenBook::operator>(const Book& rhsBook) const {
-    bool holder = false;
+bool ChildrenBook::operator>(const Book &rhsBook) const {
+  bool holder = false;
 
-    // cast book to children's book 
-    const ChildrenBook& rhsBookCasted = static_cast<const ChildrenBook&>(rhsBook);
+  // cast book to children's book
+  const ChildrenBook &rhsBookCasted =
+      static_cast<const ChildrenBook &>(rhsBook);
 
-    // compare titles, adjust bool as needed
-    (getTitle() > rhsBookCasted.getTitle()) ? holder = true : holder = false;
+  // compare titles, adjust bool as needed
+  (getTitle() > rhsBookCasted.getTitle()) ? holder = true : holder = false;
 
-    // if the book titles are the same compare authors
-    if (getTitle() == rhsBookCasted.getTitle()) {
-        (getAuthor() > rhsBookCasted.getAuthor()) ? holder = true : holder = false;
-    }
-    return holder;
+  // if the book titles are the same compare authors
+  if (getTitle() == rhsBookCasted.getTitle()) {
+    (getAuthor() > rhsBookCasted.getAuthor()) ? holder = true : holder = false;
+  }
+  return holder;
 }
 
 //---------------------------------------------------------------------------
 // overloaded operator==
-bool ChildrenBook::operator==(const Book& rhsBook) const {
-    bool holder = false; 
+bool ChildrenBook::operator==(const Book &rhsBook) const {
+  bool holder = false;
 
-    // cast rhs book to children's book
-    const ChildrenBook& rhsBookCasted = static_cast<const ChildrenBook&>(rhsBook);
+  // cast rhs book to children's book
+  const ChildrenBook &rhsBookCasted =
+      static_cast<const ChildrenBook &>(rhsBook);
 
-    // compare author, title, and year between two books 
-    if (rhsBookCasted.getAuthor() == getAuthor() && rhsBookCasted.getTitle() == getTitle() && rhsBookCasted.getYear() == getYear()) {
-        holder = true; 
-    }
-    return holder;
+  // compare author, title, and year between two books
+  if (rhsBookCasted.getAuthor() == getAuthor() &&
+      rhsBookCasted.getTitle() == getTitle() &&
+      rhsBookCasted.getYear() == getYear()) {
+    holder = true;
+  }
+  return holder;
 }
 
 //---------------------------------------------------------------------------
-// overloaded operator != 
-bool ChildrenBook::operator!=(const Book& rhsBook) const {
-    // use comparison operator to return value
-    return !(*this == rhsBook);
+// overloaded operator !=
+bool ChildrenBook::operator!=(const Book &rhsBook) const {
+  // use comparison operator to return value
+  return !(*this == rhsBook);
 }
--- childrenbook.h
+++ childrenbook.h
@@ -9,135 +9,148 @@
 //  -- initial character in input dictates 'c' for children
 //  -- Comparison operators will only be used with other ChildrenBook
 // Implementation:
-//  -- 
+//  --
 //---------------------------------------------------------------------------
 #include "book.h"
 #include <iostream>
 
 class ChildrenBook : public Book {
-    //---------------------------------------------------------------------------
-     /**
-      * @brief Output operator overload for ChildrenBook object. Dictates output
-      * formatting and functionality for a children book.
-      *
-      * @return ostream& containing information to be output in a formatted style
-      * @pre: Assumes valid childrenBook object being passed in.
-      * @post: Data not changed, but is output.
-      */
-    friend ostream& operator<<(ostream&, const ChildrenBook&);
+  //---------------------------------------------------------------------------
+  /**
+   * @brief Output operator overload for ChildrenBook object. Dictates output
+   * formatting and functionality for a children book.
+   *
+   * @return ostream& containing information to be output in a formatted style
+   * @pre: Assumes valid childrenBook object being passed in.
+   * @post: Data not changed, but is output.
+   */
+  friend ostream &operator<<(ostream &, const ChildrenBook &);
 
 public:
-    /**
-     * @brief Construct a new Children object. Creates new Children book with
-     * title, author, and amount of books owned by library.
-     *
-     * @param title is the title of the book being created
-     * @param author is the author of the book being created
-     * @param year the publishing year
-     * @param totalTitlesOwned dictates the total amount of this book owned
-     * by library
-     * @pre: assumes object needs to be created
-     * @post: children object will be destroyed, with memory freed
-     */
-    ChildrenBook();
+  /**
+   * @brief Construct a new Children object. Creates new Children book with
+   * title, author, and amount of books owned by library.
+   *
+   * @param title is the title of the book being created
+   * @param author is the author of the book being created
+   * @param year the publishing year
+   * @param totalTitlesOwned dictates the total amount of this book owned
+   * by library
+   * @pre: assumes object needs to be created
+   * @post: children object will be destroyed, with memory freed
+   */
+  ChildrenBook();
 
-    //---------------------------------------------------------------------------
-     /**
-      * @brief Destroy the Children Book object.
-      *
-      * @pre: Assumes valid ChildrenBook object is created.
-      * @post: ChildrenBook object will be deleted, with memory freed.
-      */
-    ~ChildrenBook();
+  //---------------------------------------------------------------------------
+  /**
+   * @brief Destroy the Children Book object.
+   *
+   * @pre: Assumes valid ChildrenBook object is created.
+   * @post: ChildrenBook object will be deleted, with memory freed.
+   */
+  ~ChildrenBook();
 
-    //---------------------------------------------------------------------------
-     /** buildBook
-    * @brief creates book object reading from data file
-    * @pre: A correctly formatted open data file
-    * @post: a new book object
-    */
-    bool buildBook(istream& in);
+  //---------------------------------------------------------------------------
+  /** buildBook
+   * @brief creates book object reading from data file
+   * @pre: A correctly formatted open data file
+   * @post: a new book object
+   */
+  bool buildBook(istream &in);
 
-    //---------------------------------------------------------------------------
-     /** create
-      * @brief Creates a children book object, then returns a copy of that object
-      * for the factory to function properly.
-      *
-      * @return ChildrenBook copy of newly created book object
-      * @post: new children book object is created and returned.
-      */
-    virtual Book* create();
+  //---------------------------------------------------------------------------
+  /** create
+   * @brief Creates a children book object, then returns a copy of that object
+   * for the factory to function properly.
+   *
+   * @return ChildrenBook copy of newly created book object
+   * @post: new children book object is created and returned.
+   */
+  virtual Book *create();
 
-    /** setAuthor
-     * @brief sets the book author
-     * @pre a new book object
-     * @post book author stored in data member
-     */
-    void setAuthor(string author);
+  /** setAuthor
+   * @brief sets the book author
+   * @pre a new book object
+   * @post book author stored in data member
+   */
+  void setAuthor(string author);
 
-    /** getAuthor
-     * @brief gets and returns the book author
-     * @pre book object has set author
-     * @post string of authro is returned
-     */
-    string getAuthor() const;
+  /** getAuthor
+   * @brief gets and returns the book author
+   * @pre book object has set author
+   * @post string of authro is returned
+   */
+  string getAuthor() const;
 
-    /** setYear
-     * @brief sets the year data member
-     * @pre a new book object
-     * @post a book object with a published year
-     */
-    void setYear(int year);
+  /** setYear
+   * @brief sets the year data member
+   * @pre a new book object
+   * @post a book object with a published year
+   */
+  bool setYear(int year);
 
-    /** getYear
-     * @brief returns the book published year
-     * @pre a book object with set year
-     * @post int year is returned
-     */
-    int getYear() const;
+  /** getYear
+   * @brief returns the book published year
+   * @pre a book object with set year
+   * @post int year is returned
+   */
+  int getYear() const;
 
-    /**
-    * Less than operator. Compares LHS and RHS for lesser value.
-    * @param rhsBook rhs Book being passed in
-    * @return true
-    * @return false
-    */
-   bool operator<(const Book &rhsBook) const;
+  /**
+   * Less than operator. Compares LHS and RHS for lesser value.
+   * @param rhsBook rhs Book being passed in
+   * @return true
+   * @return false
+   */
+  bool operator<(const Book &rhsBook) const;
 
-   /**
-     *
-     * Greater than operator. compares LHS and RHS for greater value.
-     * @param rhsBook rhs Book being passed in for comparison
-     * @return true
-     * @return false
-     */
-   bool operator>(const Book &rhsBook) const;
+  /**
+   *
+   * Greater than operator. compares LHS and RHS for greater value.
+   * @param rhsBook rhs Book being passed in for comparison
+   * @return true
+   * @return false
+   */
+  bool operator>(const Book &rhsBook) const;
 
-   /**
-     * Equals operator. Determines if two Book objects are equal.
-     *
-     * @param rhsBook rhs Book being passed in for equals comparison
-     * @return true
-     * @return false
-     */
-   bool operator==(const Book &rhsBook) const;
+  /**
+   * Equals operator. Determines if two Book objects are equal.
+   *
+   * @param rhsBook rhs Book being passed in for equals comparison
+   * @return true
+   * @return false
+   */
+  bool operator==(const Book &rhsBook) const;
 
-   /**
-     * Does not equals operator. Determines if two books are not equal.
-     *
-     * @param rhsBook
-     * @return true
-     * @return false
-     */
-   bool operator!=(const Book &rhsBook) const;
+  /**
+   * Does not equals operator. Determines if two books are not equal.
+   *
+   * @param rhsBook
+   * @return true
+   * @return false
+   */
+  bool operator!=(const Book &rhsBook) const;
 
-  void display() const; 
+  /**
+   * @brief Displays the information of the current book in a sorted fashion
+   * 
+   */
+  void display() const;
+
+  /**
+   * @brief Builds the data for a blank book object. Handles the "filling out"
+   * of the book being created. 
+   * 
+   * @param infile Incoming information from file. 
+   * @return true 
+   * @return false 
+   */
+  bool buildBook(istream &infile);
+
 private:
-    // author of the book, last then first
-    string author_;
+  // author of the book, last then first
+  string author_;
 
-    //publishing year
-    int year_;
-
-
+  // publishing year
+  int year_;
 };
